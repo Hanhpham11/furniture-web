a9ad5b687a189c4cc1aa62b0ff780e1508184fcc
Thu12345A <142224794+Thu12345A@users.noreply.github.com>
2023-08-23 18:55:05 +0700

    add

--- src/component/Chech out/Checkout.js
+++ src/component/Chech out/Checkout.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Header from '../Header';
 import Checkout1 from './Checkout1';
 import Checkout2 from './Checkout2';
+import Contact3 from '../Contact Us/Contact3';
+import Footer from '../Footer';
 const Checkout = () => {
   return (
     <div>
       <Header />
       <Checkout1 />
       <Checkout2 />
+      <Contact3 />
+      <Footer />
     </div>
   );
 };
